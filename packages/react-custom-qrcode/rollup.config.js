6c57a95cb4f0c2dafd4ee54f0e0127409b989a58
Koen Vlaswinkel <koen@vlaswinkel.info>
2020-09-17 16:56:43 +0200

    Generate type declarations

--- packages/react-custom-qrcode/rollup.config.js
+++ packages/react-custom-qrcode/rollup.config.js
@@ -1,6 +1,8 @@
 import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import * as path from 'path';
 
 import pkg from './package.json';
 
@@ -8,32 +10,57 @@ const extensions = [
     '.js', '.jsx', '.ts', '.tsx',
 ];
 
+const external = ['react', 'qrcode'];
+const commonPlugins = [
+    resolve({extensions, browser: true}),
+    commonjs(),
+];
+const babelPlugin = babel({
+    extensions,
+    babelHelpers: 'bundled',
+    plugins: ['@babel/plugin-proposal-optional-chaining'],
+    presets: [
+        ['@babel/preset-env', {
+            loose: true,
+            modules: false,
+            targets: '>1%, not dead, not ie 11, not op_mini all'
+        }],
+        '@babel/preset-typescript',
+        '@babel/preset-react'
+    ]
+});
+
 const config = {
-    input: 'src/index.tsx',
-    output: [
-        {
-            file: pkg.main,
-            format: 'cjs',
-        }, {
-            file: pkg.module,
-            format: 'es',
-        },
-    ],
+    input: 'react-custom-qrcode.ts',
+    output: {
+        file: pkg.main,
+        format: 'cjs',
+    },
     external: ['react', 'qrcode'],
     plugins: [
-        resolve({extensions}),
-        commonjs(),
-        babel({
-            extensions,
-            babelHelpers: 'runtime',
-            plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-proposal-optional-chaining'],
-            presets: [
-                ['@babel/preset-env', { loose: true, modules: false, targets: '>1%, not dead, not ie 11, not op_mini all' }],
-                '@babel/preset-typescript',
-                '@babel/preset-react'
-            ]
-        })
+        ...commonPlugins,
+        babelPlugin,
     ]
 };
 
-export default config;
+const declarationConfig = {
+    input: 'react-custom-qrcode.ts',
+    output: [
+        {
+            dir: path.dirname(pkg.module),
+            format: 'es',
+        },
+    ],
+    external,
+    plugins: [
+        ...commonPlugins,
+        typescript({
+            tsconfig: 'tsconfig.build.json',
+            declaration: true,
+            declarationDir: path.dirname(pkg.module),
+        }),
+        babelPlugin,
+    ]
+};
+
+export default [config, declarationConfig];
